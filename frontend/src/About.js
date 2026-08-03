4a7ee34273f957f000d1557885679914cbd5ab4c
Soumitra-32 <soumitradhaka@gmai.com>
2026-08-03 17:13:37 +0600

    modify webpage

---
+++ frontend/src/About.js
@@ -0,0 +1,39 @@
+```jsx
+function About() {
+    return (
+        <div
+            style={{
+                padding: "40px",
+                maxWidth: "800px",
+                margin: "0 auto",
+                textAlign: "center",
+                lineHeight: "1.8",
+            }}
+        >
+            <h1>Welcome to JU CSE!</h1>
+
+            <p>
+                We are proud students of the Department of Computer Science and
+                Engineering, Jahangirnagar University. This project is the
+                result of our teamwork, dedication, and passion for software
+                development.
+            </p>
+
+            <h2>Project Team</h2>
+
+            <div style={{ textAlign: "left", display: "inline-block" }}>
+                <p><strong>Project Manager:</strong> Soumitra Saha</p>
+                <p><strong>Frontend Engineer:</strong> Sukonya Dutta Pushpa</p>
+                <p><strong>Backend Engineer:</strong> Sadikur Rahman Khan</p>
+                <p><strong>SQA Engineer:</strong> Wasik Ahmed</p>
+            </div>
+
+            <p style={{ marginTop: "30px" }}>
+                Thank you for visiting our website!
+            </p>
+        </div>
+    );
+}
+
+export default About;
+```
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './About';
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <div
+          style={{
+            position: "absolute",
+            top: "20px",
+            left: "20px"
+          }}
+        >
+          <Link
+            to="/about"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              fontSize: "20px",
+              border: "1px solid white",
+              padding: "8px 16px",
+              borderRadius: "5px"
+            }}
+          >
+            About
+          </Link>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,8 +45,18 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
